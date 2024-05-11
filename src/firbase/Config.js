0c26eb6c416021a9a5886cb90c6c177889f1a4ad
M FEBRI ARDIANSYAH <131425009+FebriArdiansyah27@users.noreply.github.com>
2024-05-11 10:03:20 +0700

    Update Config.js

--- src/firbase/Config.js
+++ src/firbase/Config.js
@@ -5,9 +5,9 @@ import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyCHoFOKnPTRleYhejTxolUneswT_anE5Ys",
-  authDomain: "smallershop-1a1c3.firebaseapp.com",
-  projectId: "smallershop-1a1c3",
-  storageBucket: "smallershop-1a1c3.appspot.com",
+  authDomain: "shinShop-1a1c3.firebaseapp.com",
+  projectId: "shinShop-1a1c3",
+  storageBucket: "shinShop-1a1c3.appspot.com",
   messagingSenderId: "194191002589",
   appId: "1:194191002589:web:06c0b67d6a948867504434",
   measurementId: "G-JSCKGQL3G9",
